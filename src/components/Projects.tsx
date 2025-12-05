f77843a6f7b39089fe5a3a667041ab4575c80392
unknown <naima.adardor@edu.uiz.ac.ma>
2025-12-05 12:03:33 +0100

    test

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -138,6 +138,12 @@ const projects = [
     images: [projet8],
     ifcFile: null,
   },
+  {
+    title: "projet 9",
+    description: "",
+    images: [],
+    ifcFile: "/models/ifc.gltf", // ✅ Fichier glTF converti depuis IFC
+  },
 
 
 
